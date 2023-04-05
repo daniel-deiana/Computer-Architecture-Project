b8ccce46d257df1268b29f0280cd9d6b9a9b8bf8
Daniel Deiana <d.deiana1@studenti.unipi.it>
2023-04-05 16:44:15 +0200

    Added gitignore file

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,7 @@ for n_thread in {1..4}
 do
     for n_rep in {1..30}
     do
-        ./"create_input" input.txt 512 512 kernel.txt 2 2 $n_rep
+        ./"create_input" input.txt 1024 1024 kernel.txt 2 2 $n_rep
         ./"conv" input.txt kernel.txt $n_thread >> "data_$n_thread.csv"
     done
 done
